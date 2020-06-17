8407b4249af94b50ec0867785a66032b9a9823c0
Hugo Ala Yagi <65855944+halayagi@users.noreply.github.com>
2020-06-16 23:04:30 -0300

    Add files via upload

---
+++ desafios/desafio01-2.js
@@ -0,0 +1,29 @@
+// LaunchBase 
+// Desafio 1-2: Lidando com objetos e vetores
+// Construção e impressão de objetos
+
+const empresa = {
+    nome: 'Rocketseat',
+    cor: 'Roxo',
+    foco: 'Programação',
+    endereco: {
+        rua: 'Rua Guilherme Gembala',
+        numero: 260
+    }
+}
+
+console.log(`A empresa ${empresa.nome} está localizada em ${empresa.endereco.rua}, ${empresa.endereco.numero}`)
+
+// Vetores e objetos
+
+const programador = {
+    nome: 'Carlos',
+    idade: 32,
+    tecnologias: [
+        { nome: 'C++', especialidade: 'Desktop' },
+        { nome: 'Python', especialidade: 'Data Science' },
+        { nome: 'JavaScript', especialidade: 'Web/Mobile' }
+    ]
+}
+
+console.log(`O usuário ${programador.nome} tem ${programador.idade} anos e usa a tecnologia ${programador.tecnologias[0].nome} com especialidade em ${programador.tecnologias[0].especialidade}`)
